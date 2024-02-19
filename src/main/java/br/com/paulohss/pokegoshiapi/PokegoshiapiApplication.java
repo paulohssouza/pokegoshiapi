a2bff048dcd5737b073150c3de3182836510bfd4
Paulo Henrique <paulohss.souza@gmail.com>
2024-02-19 15:37:55 -0300

    Início do projeto PokeGoshi. Uma api que simula um tamagoshi com pokemons.

---
+++ src/main/java/br/com/paulohss/pokegoshiapi/PokegoshiapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulohss.pokegoshiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokegoshiapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokegoshiapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/paulohss/pokegoshiapi/PokegoshiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulohss.pokegoshiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokegoshiapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
